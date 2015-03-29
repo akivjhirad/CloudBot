8457eb9c0c99f7e9992561054035d28148453aa2
akivjhirad <akivjh@live.com>
2015-03-29 19:29:59 -0400

    Update OutputMyoToText.m

--- OutputMyoToText.m
+++ OutputMyoToText.m
@@ -2,17 +2,13 @@
 % first serial test with the Esduino
 
 s = serial('COM4');     %choose your COM port
-set(s, 'Baudrate', 9600);   %sets the Baudrate to the same thing as your Esduino
-s.Terminator = 'CR';
-%set (s, 'Timeout', 3000);   %I think this help make things look pretty
+set(s, 'Baudrate', 9600);   %sets the Baudrate to the same thing as your uC
+s.Terminator = 'CR';    %terminator has to match the serial output device
 fopen(s);   %open the port
 
 myoRead = ' ';
 myoRead2 = 0;
 fid = fopen('Output.txt', 'w');
-
-% t = [1:200];   
-% t = t(:);   %I used this to graph a time of 200s
 
 for i=0:100
     myoRead = fscanf(s);  %read the first 200 bits from the esduino
@@ -23,11 +19,7 @@ end
 
 fprintf(fid, '\n');
 
-
-
-%plot(t, A); %plot esduino output vs time
-
 fclose(fid);
-fclose(s);  %CLOSE THE PORT OR ELSE THINGS GET MESSED
+fclose(s);
 delete(s);  
 clear s;   
